f06b726af56c5a90183239ff315a3dcff4776233
Venki1402 <saivenkatesh.alampally@gmail.com>
2025-09-22 12:16:14 +0530

    feat: done

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,45 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to the game of Snakes and Ladders!");
+        Map<Integer, Integer> jumps = new HashMap<>();
+        // ladders
+        jumps.put(3, 22);
+        jumps.put(5, 8);
+        jumps.put(11, 26);
+        // snakes
+        jumps.put(27, 1);
+        jumps.put(21, 9);
+        jumps.put(17, 4);
 
-        Player player1 = new HumanPlayer("Player 1");
-        Player player2 = new AIPlayer("Player 2");
+        Board board = GameBuilder.createBoard(30, jumps);
 
-        System.out.println(player1.shortDesc());
-        System.out.println(player2.shortDesc());
+        List<Player> players = new ArrayList<>();
+        players.add(new AIPlayer("Alice"));
+        // players.add(new AIPlayer("Bob"));
+        // players.add(new AIPlayer("Carol"));
+        players.add(new HumanPlayer("venki"));
+
+        Dice dice = new StandardDice(6, 12345L);
+
+        StartStrategy startStrategy = new NeedsSixToStart(6);
+        EndStrategy endStrategy = new ExactFinish();
+        PenaltyStrategy penaltyStrategy = new KillOnOverlap(0);
+        TurnStrategy turnStrategy = new ExtraTurnOnSix(6);
+
+        Game game = new GameBuilder()
+                .withBoard(board)
+                .withPlayers(players)
+                .withDice(dice)
+                .withStartStrategy(startStrategy)
+                .withEndStrategy(endStrategy)
+                .withPenaltyStrategy(penaltyStrategy)
+                .withTurnStrategy(turnStrategy)
+                .build();
+
+        game.play();
     }
 }
